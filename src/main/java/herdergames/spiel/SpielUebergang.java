31d89575be9b783f8c9890e4516ebca0110731cb
Lama06 <andreasprues36@gmail.com>
2023-05-19 15:18:45 +0200

    Übergänge updaten

--- src/main/java/herdergames/spiel/SpielUebergang.java
+++ src/main/java/herdergames/spiel/SpielUebergang.java
@@ -5,12 +5,12 @@ import herdergames.video.Video;
 import java.util.Objects;
 
 public record SpielUebergang(Video video, int frame) {
-    public static final SpielUebergang B008 = new SpielUebergang(Video.UEBERGANG_B008, 0);
-    public static final SpielUebergang B107 = new SpielUebergang(Video.UEBERGANG_B107, 0);
-    public static final SpielUebergang E202 = new SpielUebergang(Video.UEBERGANG_E202, 0);
-    public static final SpielUebergang E901 = new SpielUebergang(Video.UEBERGANG_E901, 0);
-    public static final SpielUebergang F106 = new SpielUebergang(Video.UEBERGANG_F106, 0);
-    public static final SpielUebergang F109 = new SpielUebergang(Video.UEBERGANG_F109, 0);
+    public static final SpielUebergang B008 = new SpielUebergang(Video.UEBERGANG_B008, 398);
+    public static final SpielUebergang B107 = new SpielUebergang(Video.UEBERGANG_B107, 349);
+    public static final SpielUebergang E202 = new SpielUebergang(Video.UEBERGANG_E202, 199);
+    public static final SpielUebergang E901 = new SpielUebergang(Video.UEBERGANG_E901, 398);
+    public static final SpielUebergang F106 = new SpielUebergang(Video.UEBERGANG_F106, 299);
+    public static final SpielUebergang F109 = new SpielUebergang(Video.UEBERGANG_F109, 349);
     public static final SpielUebergang BANK = new SpielUebergang(Video.UEBERGANG_BANK, 0);
 
     public SpielUebergang {
